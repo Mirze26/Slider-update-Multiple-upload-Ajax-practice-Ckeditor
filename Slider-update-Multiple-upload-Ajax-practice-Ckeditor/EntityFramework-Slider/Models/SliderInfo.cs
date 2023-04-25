6f77e1807d21d1fd737728a114f81c3fcfa18fd2
Mirze26 <mirzaab@code.edu.az>
2023-04-26 00:19:09 +0400

    Slider-update-Multiple-upload-Ajax

---
+++ Slider-update-Multiple-upload-Ajax-practice-Ckeditor/EntityFramework-Slider/Models/SliderInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EntityFramework_Slider.Models
+{
+    public class SliderInfo:BaseEntity
+    {
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string? SignatureImage { get; set; }
+        [Required(ErrorMessage = "Don't be empty")]
+        [NotMapped]                                       
+        public IFormFile Photo { get; set; }             
+
+    }
+}
